e80ea6e35974db360d90bccfdaf0c019da483b33
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-20 05:14:32 -0800

    initial commit

---
+++ main.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * main - Simple shell.
+ *
+ * Return: Success.
+ */
+
+int main(void)
+{
+	while (1)
+	{
+		char *hay = NULL;
+		size_t n = 0;
+		char **arr;
+
+		printf("#cisfun$ ");
+		if (getline(&hay, &n, stdin) == EOF)
+		{
+			printf("\n");
+			break;
+		}
+		arr = cut_string(hay, arr);
+		pid_t id = fork();
+
+		if (id == 0)
+		{
+			if (execve(arr[0], arr, environ) == -1)
+			{
+				perror("./shell");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+extern char **environ;
+char **cut_string(char *hay, char **arr);
+
+#endif
