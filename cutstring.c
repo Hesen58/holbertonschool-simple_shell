d263e78cf257e2fbe18db1f9cf55fb79bff7399a
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-21 06:01:05 -0800

    hot fix

--- cutstring.c
+++ cutstring.c
@@ -11,10 +11,13 @@ char **cut_string(char *hay, char **arr)
 {
 	char *tok;
 	int i = 0;
+	int k;
 
 	tok = strtok(hay, " \n");
 
 	arr = (char **)malloc(sizeof(char *) * 250);
+	for (k = 0; k < 250; k++)
+		arr[k] = NULL;
 
 	while (tok != NULL)
 	{
