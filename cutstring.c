a1e118a1d219aa90cb833afb866038ad5237a370
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-27 05:53:01 -0800

    hot fix

---
+++ cutstring.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cut_string - Create new array with strtok
+ * @buf: user input.
+ * @arr: array.
+ *
+ * Return: 2d array.
+ */
+
+char **cut_string(char *buf, char **arr)
+{
+	char *tok;
+	int i = 0;
+	int k;
+
+	tok = strtok(buf, " \n\t");
+	if (tok == NULL)
+	{
+		return (NULL);
+	}
+	arr = (char **)malloc(sizeof(char *) * 250);
+	for (k = 0; k < 250; k++)
+		arr[k] = NULL;
+	while (tok != NULL)
+	{
+		if (strlen(tok) > 0)
+		{
+			arr[i] = (char *)malloc(strlen(tok) + 1);
+			strcpy(arr[i], tok);
+			i++;
+		}
+		tok = strtok(NULL, " \n\t");
+	}
+	arr[i] = NULL;
+	return (arr);
+}
---
+++ main.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+ * main - Simple shell.
+ *
+ * Return: Success.
+ */
+
+int main(void)
+{
+	int status = 0;
+	while (1)
+	{
+		char *buf = NULL;
+		size_t n = 0;
+		char **arr;
+		pid_t id;
+
+		if (isatty(STDIN_FILENO))
+		{
+			printf("#cisfun$ ");
+		}
+
+		if (getline(&buf, &n, stdin) == -1)
+		{
+			if (isatty(STDIN_FILENO))
+					printf("\n");
+			free(buf);
+			break;
+		}
+		if (environ == NULL)
+		{
+			free(buf);
+			exit(127);
+		}
+		arr = cut_string(buf, arr);
+
+		id = fork();
+
+		if (id == 0)
+		{
+				free(buf);
+				execvp(arr[0], arr);
+				fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
+                        	free_arr(arr);
+				exit(127);
+		}
+		else
+		{
+			            waitpid(id, &status, 0);
+
+            		if (WIFEXITED(status))
+                		status = WEXITSTATUS(status);
+           	 	else
+                		status = 1;
+		}
+		free_arr(arr);
+		free(buf);
+	}
+	return (status);
+}
