44d9e0a4b672900ac509a96df7189d86a1689e59
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2023-12-27 05:00:34 -0800

    hot fix

--- main.c
+++ main.c
@@ -34,14 +34,18 @@ int main(void)
 		{
 				free(buf);
 				execvp(arr[0], arr);
-				status = 127;
 				fprintf(stderr, "./hsh: 1: %s: not found\n", arr[0]);
                         	free_arr(arr);
-				break;
+				exit(127);
 		}
 		else
 		{
-			wait(NULL);
+			waitpid(id, &status, 0);
+
+			if (WIFEXITED(status))
+				status = WEXITSTATUS(status);
+			else
+				status = 1;
 		}
 		free_arr(arr);
 		free(buf);
